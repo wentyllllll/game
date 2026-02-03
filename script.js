320bec5f7492eb5d6b522776b335841b362d03cb
went <259191644+wentyllllll@users.noreply.github.com>
2026-02-03 22:33:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -36,8 +36,9 @@ yesBtn.addEventListener("click", () => {
 
   document.body.innerHTML = `
     <div style="height:100dvh; display:flex; flex-direction:column; justify-content:center; align-items:center; background:#ffb6c1; text-align:center; padding:20px;">
-      <h1 style="font-size:30px; color:#d6336c;">YAYYY SHE SAID YESSS!<br>I LOVE YOU DZESIKAAAAAAAA ❤️❤️❤️</h1>
+      <h1 style="font-size:30px; color:#d6336c;">YAYYY YOU SAID YESS!!!<br>I LOVE YOU DZESIKAAAAAAAA ❤️❤️❤️</h1>
       <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" style="width:80%; max-width:300px; border-radius:15px; margin-top:20px;">
     </div>
   `;
 });
+
